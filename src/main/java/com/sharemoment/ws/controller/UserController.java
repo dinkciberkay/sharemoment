f1ab8bd513b4162ac1c77e8e7618957d39b74afa
Berkay Laptop <dinkciberkay@gmail.com>
2020-09-29 22:26:03 +0300

    Spring Security ile birlikte Hata Mesajları için ortak bir çözüm yapılarak, tüm hata mesajları ApiError tipine getirildi.

--- src/main/java/com/sharemoment/ws/controller/UserController.java
+++ src/main/java/com/sharemoment/ws/controller/UserController.java
@@ -33,15 +33,16 @@ public class UserController {
         return userService.createNewUser(userDto);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-        ApiError apiError = new ApiError(400, "Validation Error", "/api/users");
-        Map<String, String> validationErrors = new HashMap<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        apiError.setValidationErrors(validationErrors);
-        return apiError;
-    }
+    //Spring Security'e bırakıyoruz. Hata cevaplarını
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+//        ApiError apiError = new ApiError(400, "Validation Error", "/api/users");
+//        Map<String, String> validationErrors = new HashMap<>();
+//        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+//            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+//        }
+//        apiError.setValidationErrors(validationErrors);
+//        return apiError;
+//    }
 }
--- src/main/java/com/sharemoment/ws/error/ErrorHandler.java
+++ src/main/java/com/sharemoment/ws/error/ErrorHandler.java
@@ -3,16 +3,21 @@ package com.sharemoment.ws.error;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class ErrorHandler implements ErrorController {
 
     //Spring Security ile Kimlik Doğrulama Hata Cevabı
+
+    //UYGULAMA ÇAPINDA TEK BİR YERDE BÜTÜN ERRORLARI APIERROR A ÇEVİRİYOR DURUMA GELDİK.
 
     @Autowired
     private ErrorAttributes errorAttributes;
@@ -23,7 +28,22 @@ public class ErrorHandler implements ErrorController {
         String message = (String) attributes.get("message");
         String path = (String) attributes.get("path");
         int status = (Integer) attributes.get("status");
-        return new ApiError(status, message, path);
+        ApiError apiError = new ApiError(status, message, path);
+        //Message path ve status gibi Validation Fieldlarla ilgili Error Listesini de alabiliyoruz.
+        //Ve aldığımız listeyi ApiErrors içerisindeki ValidationErrors mapine dönüştüreceğiz.
+        if (attributes.containsKey("errors")) {// NULL POINTER EXCEPTION
+            List<FieldError> fieldErrors = (List<FieldError>) attributes.get("errors");
+            Map<String, String> validationErrors = new HashMap<>();
+            for (FieldError fieldError : fieldErrors) {
+                validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            apiError.setValidationErrors(validationErrors);
+        }
+        return apiError;
+
+        //BÖYLECE @EXCEPTIONHANDLER 'A GEREK KALMADAN, HEM ERROR CONTROLLER VASITASIYLA
+        //HEM AUTHENTICATIONDAKI ERRORLARI API ERROR A MODELLEYEN
+        //HEM DE VALIDATIONLI FAIL DURUMLARINI APIERRORA MODELLEYEN METOT OLDU.
     }
 
 
